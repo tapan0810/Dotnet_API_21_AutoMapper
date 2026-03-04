882828ae8eb202d6e8a92b633f95ebccf4d77f67
tapan0810 <arceus0810@gmail.com>
2026-03-05 00:29:23 +0530

    feat: Implemnetde Service using AutoMapper

---
+++ Dotnet_API_21_/Service/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using Dotnet_API_21_.Entities.Dtos;
+
+namespace Dotnet_API_21_.Service
+{
+    public interface IStudentService
+    {
+        public Task<List<GetAllStudentsDto>> GetAllStudents();
+        public Task<GetStudentByIdDto> GetStudentById(int studentId);
+        public Task<GetStudentByIdDto> CreateStudent(CreateStudentDto student);
+        public Task<bool> UpdateStudent(int id,UpdateStudentDto student);
+        public Task<bool> DeleteStudent(int id);
+    }
+}
---
+++ Dotnet_API_21_/Service/StudentService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Dotnet_API_21_.Data;
+using Dotnet_API_21_.Entities.Dtos;
+using Dotnet_API_21_.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_API_21_.Service
+{
+    public class StudentService(IMapper _mapper, StudentDbContext _context) : IStudentService
+    {
+        public async Task<GetStudentByIdDto?> CreateStudent(CreateStudentDto student)
+        {
+            var stud = _mapper.Map<Student>(student);
+             _context.DbSet.Add(stud);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<GetStudentByIdDto>(stud);
+        }
+
+        public async Task<bool> DeleteStudent(int id)
+        {
+            var stud =await  _context.DbSet.FirstOrDefaultAsync(x=>x.StudentId == id);
+
+            if(stud is null) return false;
+
+            _context.DbSet.Remove(stud);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<List<GetAllStudentsDto>> GetAllStudents()
+        {
+            var students = await _context.DbSet.ToListAsync();
+
+            return _mapper.Map<List<GetAllStudentsDto>>(students);
+        }
+
+        public async Task<GetStudentByIdDto> GetStudentById(int studentId)
+        {
+            var stud = await _context.DbSet.FirstOrDefaultAsync(X => X.StudentId == studentId);
+
+            if (stud is null) return null;
+
+            return _mapper.Map<GetStudentByIdDto>(stud);
+        }
+
+        public async Task<bool> UpdateStudent(int id, UpdateStudentDto student)
+        {
+            var stud = await _context.DbSet.FirstOrDefaultAsync(x => x.StudentId == id);
+
+            if(stud is null) return false;
+
+            _mapper.Map(student, stud);
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
